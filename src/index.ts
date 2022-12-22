f1c29695de7b08f6530ba03da526fabb59cadc3b
Matheus Tanaka <matheustanakanog@gmail.com>
2022-12-22 14:35:29 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Init")
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello World <h1/>");
+});
+
+app.listen(PORT, () =>
+  console.log(`The server is listening on http://localhost:${PORT}/`)
+);
